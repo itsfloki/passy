a5c33772613bb0f87733be10f7c8a45fe191af52
itsfloki <coder.abhijit90@gmail.com>
2023-10-21 08:42:50 +0530

    update: handle inventory file

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import os
 import sys
+import json
 from config import Config
 from manager import Manager
+
+# Constants
+INVENTORY_DIR = os.getenv("HOME") + "/.config/passy"
+INVENTORY_FILE = os.path.join(INVENTORY_DIR, "data.json")
 
 
 def configure():
     """Configure environment"""
-    config_path = os.getenv("HOME") + "/.config/passy"
-    os.makedirs(config_path, exist_ok=True)
+    os.makedirs(INVENTORY_DIR, exist_ok=True)
 
-    config_file_path = os.path.join(config_path, "data.json")
-
-    with open(config_file_path, 'w') as cf:
-        cf.write('')
-    print("Configuration added.")
+    with open(INVENTORY_FILE, 'w') as fp:
+        json.dump([], fp, sort_keys=True, indent=4)
+    print("Inventory added.")
 
 
 def generate(label: str, length: int) -> str:
     pass_manager = Manager(label)
-    return pass_manager.create(length)
+    password = pass_manager.create(length)
+    pass_obj = {"label": label, "password": password}
+    data = []
+
+    with open(INVENTORY_FILE, 'r') as fp:
+        data = json.load(fp)
+
+    with open(INVENTORY_FILE, 'w') as fp:
+        data.append(pass_obj)
+        json.dump(data, fp, sort_keys=True, indent=4)
+    return password
 
 
 def run(conf: Config):
